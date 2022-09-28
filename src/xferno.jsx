f334cac5175ebb7bdcd06c324385c487b26e6872
simz <simon.gendrin@free.fr>
2022-09-29 03:59:25 +1300

    Add useRef useMemo useLayoutEffect useReducer
    useLayoutEffect code taken from preact
    
    i got some trouble when the user remove an element using usestate i did need to check currentComponent.$LI as a wokaround
    
    most of code was badly tested but it works for me now on my complex application
    
    so i guess kinda of stable

--- src/xferno.jsx
+++ src/xferno.jsx
@@ -1,38 +1,58 @@
-import { Component } from 'inferno';
-import { eq } from './eq';
-import * as inferno from 'inferno';
-import { VNodeFlags } from 'inferno-vnode-flags';
-
-const emptyProps = {};
+import  { Component, Fragment, EMPTY_OBJ,  createComponentVNode as createComponentVNodeo, createFragment, createPortal, createRef, createRenderer, createTextVNode, createVNode, forwardRef, directClone, findDOMfromVNode, getFlagsForElementVnode, linkEvent, normalizeProps, options, render, rerender, version, 
+// Internal methods, used by hydration
+_CI, _HI, _M, _MCCC,_ME,_MFCC,_MR, _MP, __render, _RFC } from './infernopack'; 
+//import { eq } from './eq'; 
+ 
+import { VNodeFlags } from 'inferno-vnode-flags'; 
+ 
+export function shallowEqual (a, b) { 
+ 
+    for (let i in a) if (i !== '__source' && !(i in b)) return true;
+	for (let i in b) if (i !== '__source' && a[i]!==b[i]) return true;
+    return false;
+}
 
 /**
  * currentComponent tracks hook state for the currently rendering component.
  * When an xferno component is rendered, its hook state tracker becomes currentComponent.
  * When rendering is complete, the previous tracker, if any, is restored.
  */
-let currentComponent;
-
-function renderChild(component, { child, childProps }, context) {
+let currentComponent,prevTracker;
+const emptyProps={}
+function renderChild(component, {child, ...childProps}, context) {
   // Push ourselves onto the hook stack
-  const prevTracker = currentComponent;
+   prevTracker = currentComponent;
   currentComponent = component;
 
-  try {
+  try {  
+  //  console.log("flags",childProps,childProps&&!!childProps.flags & 32768 /* ForwardRef */)
     return child(childProps || emptyProps, context);
-  } finally {
-    // Pop ourselves off of the hook stack
-    currentComponent = prevTracker;
+  } finally { 
+    console.log("finqlly")
+		//if (currentComponent._pendingEffects?.length) 
+    // Pop ourselves off of the hook stack 
+    afterPaint(); 
+    //currentComponent = prevTracker?prevTracker:currentComponent;
+    currentComponent = prevTracker? prevTracker:{};
   }
 }
-
+function eq(a, b) {
+  if (a === b) {
+    return true;
+  }
+  else if (Array.isArray(a) && Array.isArray(b)) {
+    return a.length === b.length && a.every((x, i) => x === b[i]);
+  }
+ return !!b 
+}
 function createTracker(component) {
-  // Used to generate sequential hook ids for this component
-  // the sequenial requirement is why hooks can't be in conditionals.
+  // Used to generate sshallowEqualuential hook ids for this component
+  // the sshallowEqualuenial rshallowEqualuirement is why hooks can't be in conditionals.
   let nextId = 0;
 
   // The list of hooks which have been instantiated for this component,
   // this is keyed by hook id. Hook instances are used to determine if
-  // the hook value has changed, as well as to track cleanup requirements.
+  // the hook value has changed, as well as to track cleanup rshallowEqualuirements.
   const hookInstances = [];
 
   // Used if useSelector is ever invoked. We have a single Redux-like
@@ -96,7 +116,7 @@ function createTracker(component) {
     },
 
     /**
-     * Get the next hook in the hook sequence.
+     * Get the next hook in the hook sshallowEqualuence.
      * @param {*} watchList An item which is used to determine if the hook needs to be re-initialized
      */
     getHook(watchList, fn) {
@@ -104,26 +124,26 @@ function createTracker(component) {
       const value = hook.value;
       fn(hook);
       shouldUpdate = shouldUpdate || !eq(hook.value, value);
-      return hook.value;
+      return hook;
     },
 
     shouldComponentUpdate(nextProps, nextState, context) {
       // We'll always update if the child props change.
-      shouldUpdate = !eq(component.props.childProps, nextProps.childProps);
+      shouldUpdate = !shallowEqual(component.props, nextProps)
+      // ||    !shallowEqual(component.state, nextState);
 
       // This ensures that any calls to useState will get the latest state.
-      component.state = nextState;
+       component.state = nextState;
 
       // When we begin a render pass, we need to reset our hook ids,
-      // so that the sequence is consistent between render calls.
-      nextId = 0;
-
-      // We cache the result of the render, so that we don't *double* render.
-      const renderResult = renderChild(component, nextProps, context);
-
+      // so that the sshallowEqualuence is consistent between render calls.
+       nextId = 0; 
       // We only want to set this.renderResult if we are updating. Because otherwise, it's
       // a boolean (bypassing the possibly expensive v-dom generation).
-      if (shouldUpdate) {
+      if (shouldUpdate) {    
+      const renderResult = renderChild(component, nextProps, context);
+
+            // We cache the result of the render, so that we don't *double* render.
         tracker.renderResult = renderResult;
       }
 
@@ -132,6 +152,7 @@ function createTracker(component) {
 
     dispose() {
       // Clean our Redux subscription.
+      tracker.renderResult = null
       if (unsubscribeFromStore) {
         unsubscribeFromStore();
       }
@@ -148,7 +169,8 @@ function createTracker(component) {
 
 class HookComponent extends Component {
   constructor(props, context) {
-    super(props, context);
+    super(props, context); 
+    currentComponent=this
   }
 
   dispose() {
@@ -161,13 +183,14 @@ class HookComponent extends Component {
 
   componentWillReceiveProps({ child }) {
     // We're switching components, so we need to do a full reset...
-    if (child !== this.props.child) {
-      this.dispose();
+    //if (!shallowEqual(this.props.child, child)){
+    if(child !== this.props.child) {
+    this.dispose();
     }
   }
-
+ 
   componentWillUnmount() {
-    this.dispose();
+ this.dispose();
   }
 
   getHook(watchList, fn) {
@@ -175,19 +198,20 @@ class HookComponent extends Component {
       this.tracker = createTracker(this);
     }
     return this.tracker.getHook(watchList, fn);
-  }
-
-  /**
+  }  
+/** 
    * We do some tomfoolery to determine if we really need to re-render. We need
    * to run the hooks in order to know if re-rendering is necessary, and the
    * hooks only run when we *render* the child. So we actually need to render
    * the child.
    */
   shouldComponentUpdate(nextProps, nextState, context) {
+    //if(!nextProps.childProps)
+    //return true
     if (!this.tracker) {
-      return true;
+      return (!shallowEqual(this.props, nextProps)
+      ||!shallowEqual(this.state, nextState));
     }
-
     return this.tracker.shouldComponentUpdate(nextProps, nextState, context);
   }
 
@@ -196,54 +220,73 @@ class HookComponent extends Component {
   }
 }
 
-/**
- * These inferno exports are required in order for xferno to be
- * compatible with babel-plugin-inferno.
- */
-export const createVNode = inferno.createVNode;
-export const normalizeProps = inferno.normalizeProps;
-export const createTextVNode = inferno.createTextVNode;
-export const createFragment = inferno.createFragment;
 
 /**
  * Here, we override inferno's createComponentVNode if the node being created
  * is a functional component. In that case we wrap it in our HookComponent.
  */
 export function createComponentVNode(flags, type, props, key, ref) {
-  if (type.prototype instanceof inferno.Component) {
-    return inferno.createComponentVNode(flags, type, props, key, ref);
+  var l=type.prototype; 
+  if (!l) {
+    return createComponentVNodeo(flags, type, props, key, ref);
   }
-
-  return inferno.createComponentVNode(
+    if (type.prototype instanceof Component ) {
+    return createComponentVNodeo(flags, type, props, key, ref);
+  }
+   // console.log(type,flags)
+ // if ((type.prototype instanceof Function)){ 
+   //console.log("flags",!!flags & 32768 /* ForwardRef */) 
+  return createComponentVNodeo(
     VNodeFlags.ComponentUnknown,
     HookComponent,
-    { child: type, childProps: props },
+    { child: type,  ...props },
     key,
     ref,
-  );
+  ); 
 }
 
+
+/**
+ * @param {import('./index').Reducer<any, any>} reducer
+ * @param {import('./index').StateUpdater<any>} initialState
+ * @param {(initialState: any) => void} [init]
+ * @returns {[ any, (state: any) => void ]}
+ */
+function invokeOrReturn(arg, f) {
+	return typeof f == 'function' ? f(arg) : f;
+}
+export function useState(initialState) { 
+	return useReducer(invokeOrReturn, initialState);
+}
+ 
 /**
  * Returns a stateful value, and a function to update it.
  * @param {T|() => T} initialState - The initial state or a function which returns the initial state.
  * @returns {[T, (T|(T) => T)]} An array [state, setState]
  */
-export function useState(initialState) {
+export function useReducer(reducer, initialState, init) { 
+  console.log("LI",currentComponent.$LI)
+
+  //return setTimeout(()=>useReducer(reducer, initialState, init),100)
   return currentComponent.getHook(0, (hook) => {
     if (hook.isNew) {
-      currentComponent.state[hook.id] =
-        typeof initialState === 'function' ? initialState() : initialState;
-
-      const component = currentComponent;
-      hook.$setState = (setter) => {
-        const s = component.state;
-        const state = s[hook.id];
-        const nextState = typeof setter === 'function' ? setter(s[hook.id]) : setter;
+      currentComponent.state[hook.id] = 
+      !init ? invokeOrReturn(undefined, initialState) : init(initialState); 
+       const component = currentComponent;
+      hook.$setState = (setter) => {  
+        const currentstate = component.state[hook.id];
+        const nextState = reducer(currentstate, setter);
+        
+       // const nextState = typeof setter === 'function' ? setter(s[hook.id]) : setter;
         // Exit early if the state is unchanged. This means we don't
         // really support state mutation.
-        if (state === nextState) {
+        if (currentstate === nextState) { 
           return;
-        }
+        }  
+         if(!component.$LI)
+   return [, ()=>{}];
+   console.log("setState",nextState)
+    //    component.$BR=true
         // We mutate here, purely as a micro optimization, probably not necessary, but
         // the way we've written this, it's fine.
         return component.setState((s) => {
@@ -254,20 +297,118 @@ export function useState(initialState) {
     }
 
     hook.value = [currentComponent.state[hook.id], hook.$setState];
-  });
+  }).value
 }
 
+
+/** @type {Array<import('./internal').Component>} */
+let afterPaintEffects = [];
+
+let EMPTY = []; 
+
+const RAF_TIMEOUT = 100;
+let prevRaf;
+let HAS_RAF = typeof requestAnimationFrame == 'function';
+
+function afterNextFrame(callback) {
+	const done = () => {
+		clearTimeout(timeout);
+		if (HAS_RAF) cancelAnimationFrame(raf);
+		setTimeout(callback);
+	};
+	const timeout = setTimeout(done, RAF_TIMEOUT);
+
+	let raf;
+	if (HAS_RAF) {
+		raf = requestAnimationFrame(done);
+	}
+}
+/**
+ * @param {import('./internal').EffectHookState} hook
+ */
+function invokeCleanup(hook) {
+	// A hook cleanup can introduce a call to render which creates a new root, this will call options.vnode
+	// and move the currentComponent away.
+	const comp = currentComponent;
+	let cleanup = hook._cleanup;
+	if (typeof cleanup == 'function') {
+		hook._cleanup = undefined;
+		cleanup();
+	}
+
+	currentComponent = comp;
+}
+
+/**
+ * Invoke a Hook's effect
+ * @param {import('./internal').EffectHookState} hook
+ */
+function invokeEffect(hook) {
+	// A hook call can introduce a call to render which creates a new root, this will call options.vnode
+	// and move the currentComponent away. 
+	 const comp = currentComponent;
+   
+	hook._cleanup = hook.value();
+	 currentComponent = comp;
+} 
+
+function flushAfterPaintEffects() {
+	let component;
+	while ((component = afterPaintEffects.shift())) {
+		if (!component._pendingEffects) continue;
+		try {
+	 //	component._pendingEffects.forEach(invokeCleanup);
+			component._pendingEffects.forEach(invokeEffect);
+			component._pendingEffects = [];
+		} catch (e) {
+			component._pendingEffects = [];
+		}
+	}
+}
+
+// Note: if someone used options.debounceRendering = requestAnimationFrame,
+// then effects will ALWAYS run on the NEXT frame instead of the current one, incurring a ~16ms delay.
+// Perhaps this is not such a big deal.
+/**
+ * Schedule afterPaintEffects flush after the browser paints
+ * @param {number} newQueueLength
+ */
+function afterPaint(newQueueLength) {
+//	if (newQueueLength === 1 ) { 
+	 afterNextFrame(flushAfterPaintEffects);
+//	}
+}
 /**
  * Run the specified effect anytime watchList changes.
  * @param {function} fn - The effect to be run
  * @param {*} watchList - The value or array of values to be watched. fn will be re-run if this changes.
  */
-export function useEffect(fn, watchList) {
+export function useEffect(fn, watchList) {   
   return currentComponent.getHook(watchList, (hook) => {
     if (hook.isNew) {
       hook.dispose = fn();
     }
-  });
+  }).value
+}
+export function useLayoutEffect(fn, watchList) { 
+  function value(){
+    return currentComponent.getHook(watchList, (hook) => {
+    if (hook.isNew) {
+      hook.dispose = fn();
+    }
+  })}
+  const hookpending={
+  //  _cleanup:fn,
+    value
+    }
+  
+  currentComponent._pendingEffects = currentComponent._pendingEffects || [];
+		currentComponent._pendingEffects.push(hookpending);
+    afterPaintEffects.push(currentComponent)
+}
+
+export function useCallback(callback, args) { 
+	return useMemo(() => callback, args);
 }
 
 /**
@@ -277,13 +418,20 @@ export function useEffect(fn, watchList) {
  * @returns {T} the result of calling fn()
  */
 export function useMemo(fn, watchList) {
-  return currentComponent.getHook(watchList, (hook) => {
+  return  currentComponent.getHook(watchList, (hook) => {
     if (hook.isNew) {
       hook.value = fn();
     }
-  });
+  }).value
 }
+ 
+export function useRef(initialValue) {
+	return useMemo(() => ({ current: initialValue }), []);
+} 
 
+export { Component, Fragment, EMPTY_OBJ, createFragment, createPortal, createRef, createRenderer, createTextVNode, createVNode, forwardRef, directClone, findDOMfromVNode, getFlagsForElementVnode, linkEvent, normalizeProps, options, render, rerender, version, 
+// Internal methods, used by hydration
+_CI, _HI, _M, _MCCC,_ME,_MFCC,_MR, _MP, __render, _RFC }
 /**
  * Run the specified function any time watchList changes, memoize and return result.value.
  * The return result's dispose function will be called whenever fn is reinvoked and / or
@@ -299,9 +447,8 @@ export function useDisposable(fn, watchList) {
       hook.value = result.value;
       hook.dispose = result.dispose;
     }
-  });
+  }).value;
 }
-
 /**
  * Get a subset of Redux (or similar state manager store) state.
  * @param {(TState) => TResult} fn - A function taking Redux state, and returning the desired subset.
@@ -310,7 +457,7 @@ export function useDisposable(fn, watchList) {
 export function useSelector(fn) {
   return currentComponent.getHook(0, (hook) => {
     hook.value = fn(currentComponent.tracker.getStoreState());
-  });
+  }).value;
 }
 
 /**
